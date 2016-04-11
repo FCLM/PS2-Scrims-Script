b3bc0ab7555420e60377ace2426e97414bac53ad
Dylan Cross <dylnnzl@gmail.com>
2016-04-11 18:30:26 +1200

    Created objects for scoring

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,37 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'PlanetSide 2 Scrim Script' });
 });
 
+var scoreboard = {
+  teamOne: {
+    alias : '',
+    outfit_id : '',
+    name : '',
+    faction : 0,
+    points : 0,
+    netScore : 0,
+    kills : 0,
+    deaths : 0,
+    members : []
+  },
+  teamTwo: {
+    alias : '',
+    outfit_id : '',
+    name : '',
+    faction : 0,
+    points : 0,
+    netScore : 0,
+    kills : 0,
+    deaths : 0,
+    members : []
+  }
+};
+
+var killfeed = {
+  killer : '',
+  dead : '',
+  weapon : '',
+  image : '',
+  timestamp: ''
+};
+
 module.exports = router;
