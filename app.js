c885d37d125860c5c38473fa425f3866a9357753
Dylan Cross <dylnnzl@gmail.com>
2016-04-15 20:57:48 +1200

    added a basic password control

--- app.js
+++ app.js
@@ -105,11 +105,13 @@ io.on('connection', function(sock) {
   });
   sock.on('start', function (data) {
     var event = data.obj;
-    if ((event.hasOwnProperty('teamOne')) && (event.hasOwnProperty('teamTwo'))) {
-      start(event.teamOne, event.teamTwo);
-      console.log(event.teamOne + ' ' + event.teamTwo);
-    } else {
-      console.error('No data sent: ' + event.teamOne + ' ' + event.teamTwo);
+    if (event.auth == 'Password1') {
+      if ((event.hasOwnProperty('teamOne')) && (event.hasOwnProperty('teamTwo'))) {
+        start(event.teamOne, event.teamTwo);
+        console.log(event.teamOne + ' ' + event.teamTwo);
+      } else {
+        console.error('No data sent: ' + event.teamOne + ' ' + event.teamTwo);
+      }
     }
   });
 });
--- public/js/adminControls.js
+++ public/js/adminControls.js
@@ -7,28 +7,20 @@ document.addEventListener('submit', function(e) {
     e.preventDefault();
     var tagOne = $("#T1Tag").val();
     var tagTwo = $("#T2Tag").val();
+    var pw = $('#pw').val();
     var obj = {
         teamOne : tagOne,
-        teamTwo : tagTwo
+        teamTwo : tagTwo,
+        auth: pw
     };
     console.log(obj);
     socket.emit('start', { obj: obj});
 });
 socket.on('connect', function() {
-    socket.on('teams', function (data) {
-        var event = data.obj;
-        console.log(event);
-        $('#outfitT1').html('[' + event.teamOne.alias + '] ' + event.teamOne.name).addClass('faction' + event.teamOne.faction);
-        $('#outfitT2').html('[' + event.teamTwo.alias + '] ' + event.teamTwo.name).addClass('faction' + event.teamTwo.faction);
-        $('#Team1').addClass('faction' + event.teamOne.faction);
-        $('#Team2').addClass('faction' + event.teamTwo.faction);
-    });
-    socket.on ('time', function(data) {
-        var event = data.obj;
-        console.log(event);
-        $('#timer').html(event.minutes + ' : ' + event.seconds);
-    });
-    socket.on ('redirect', function(data) {
+    socket.on ('redirect', function() {
     //redirect to index.html
+        console.log('Session started, redirecting to index.html');
+        window.location.replace("localhost:3001");
     });
 });
+socket.emit('backchat', { obj: 'Admin - Web Connection' });
