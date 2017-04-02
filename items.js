67066b063c2be9baf794ac232dd9a1be13934c79
Dylan Cross <dylancross@me.com>
2017-04-02 13:53:03 +1200

    Created preset category maps for Thunderdome, Emerald and OvO

--- items.js
+++ items.js
@@ -6,11 +6,12 @@ var prequest = require('prequest');
 var Q = require('q');
 
 var items = {};
-
+var categoryMap = 0;
 //N.B: category 0 doesn't exist in the API but i assume it is for being killed by wreckage or terrain and it was annoying me with error messages so its now in the map
 // You can edit this bit to have it return whatever points you want, so this can be used for most rulesets
-var category_map = {
-  '0':   { 'category' : 'Error - Killed by Unkown',   points : 0 },
+// Briggs Thunderdome (2016) Scoring:
+var thunderDomeCategoryMap = {
+  '0':   { 'category' : 'Unknown (could be anything', points : 0 },
   '2':   { 'category' : 'Knife',                      points : 2 },
   '3':   { 'category' : 'Pistol',                     points : 2 },
   '4':   { 'category' : 'Shotgun',                    points : 0 },
@@ -34,47 +35,152 @@ var category_map = {
   '22':  { 'category' : 'AI MAX (Right)',             points : 1 },
   '23':  { 'category' : 'AA MAX (Left)',              points : 1 },
   '24':  { 'category' : 'Crossbow',                   points : 2 },
-  '99':  { 'category' : 'Camo',                       points : null },
   '100': { 'category' : 'Infantry',                   points : 1 },
-  '101': { 'category' : 'Vehicles',                   points : null },
+  '101': { 'category' : 'Vehicles',                   points : 0 },
   '102': { 'category' : 'Infantry Weapons',           points : 1 },
-  '103': { 'category' : 'Infantry Gear',              points : null },
-  '104': { 'category' : 'Vehicle Weapons',            points : null },
-  '105': { 'category' : 'Vehicle Gear',               points : null },
-  '106': { 'category' : 'Armor Camo',                 points : null },
-  '107': { 'category' : 'Weapon Camo',                points : null },
-  '108': { 'category' : 'Vehicle Camo',               points : null },
-  '109': { 'category' : 'Flash Primary Weapon',       points : null },
-  '110': { 'category' : 'Galaxy Left Weapon',         points : null },
-  '111': { 'category' : 'Galaxy Tail Weapon',         points : null },
-  '112': { 'category' : 'Galaxy Right Weapon',        points : null },
-  '113': { 'category' : 'Galaxy Top Weapon',          points : null },
-  '114': { 'category' : 'Harasser Top Gunner',        points : null },
-  '115': { 'category' : 'Liberator Belly Weapon',     points : null },
-  '116': { 'category' : 'Liberator Nose Cannon',      points : null },
-  '117': { 'category' : 'Liberator Tail Weapon',      points : null },
-  '118': { 'category' : 'Lightning Primary Weapon',   points : null },
-  '119': { 'category' : 'Magrider Gunner Weapon',     points : null },
-  '120': { 'category' : 'Magrider Primary Weapon',    points : null },
-  '121': { 'category' : 'Mosquito Nose Cannon',       points : null },
-  '122': { 'category' : 'Mosquito Wing Mount',        points : null },
-  '123': { 'category' : 'Prowler Gunner Weapon',      points : null },
-  '124': { 'category' : 'Prowler Primary Weapon',     points : null },
-  '125': { 'category' : 'Reaver Nose Cannon',         points : null },
-  '126': { 'category' : 'Reaver Wing Mount',          points : null },
-  '127': { 'category' : 'Scythe Nose Cannon',         points : null },
-  '128': { 'category' : 'Scythe Wing Mount',          points : null },
-  '129': { 'category' : 'Sunderer Front Gunner',      points : null },
-  '130': { 'category' : 'Sunderer Rear Gunner',       points : null },
-  '131': { 'category' : 'Vanguard Gunner Weapon',     points : null },
-  '132': { 'category' : 'Vanguard Primary Weapon',    points : null },
-  '133': { 'category' : 'Implants',                   points : null },
-  '134': { 'category' : 'Consolidated Camo',          points : null },
-  '135': { 'category' : 'VO Packs',                   points : null },
-  '136': { 'category' : 'Male VO Pack',               points : null },
-  '137': { 'category' : 'Female VO Pack',             points : null },
-  '138': { 'category' : 'Valkyrie Nose Gunner',       points : null },
-  '139': { 'category' : 'Engineer Turrets',           points : 1 }
+  '104': { 'category' : 'Vehicle Weapons',            points : 0 },
+  '109': { 'category' : 'Flash Primary Weapon',       points : 0 },
+  '110': { 'category' : 'Galaxy Left Weapon',         points : 0 },
+  '111': { 'category' : 'Galaxy Tail Weapon',         points : 0 },
+  '112': { 'category' : 'Galaxy Right Weapon',        points : 0 },
+  '113': { 'category' : 'Galaxy Top Weapon',          points : 0 },
+  '114': { 'category' : 'Harasser Top Gunner',        points : 0 },
+  '115': { 'category' : 'Liberator Belly Weapon',     points : 0 },
+  '116': { 'category' : 'Liberator Nose Cannon',      points : 0 },
+  '117': { 'category' : 'Liberator Tail Weapon',      points : 0 },
+  '118': { 'category' : 'Lightning Primary Weapon',   points : 0 },
+  '119': { 'category' : 'Magrider Gunner Weapon',     points : 0 },
+  '120': { 'category' : 'Magrider Primary Weapon',    points : 0 },
+  '121': { 'category' : 'Mosquito Nose Cannon',       points : 0 },
+  '122': { 'category' : 'Mosquito Wing Mount',        points : 0 },
+  '123': { 'category' : 'Prowler Gunner Weapon',      points : 0 },
+  '124': { 'category' : 'Prowler Primary Weapon',     points : 0 },
+  '125': { 'category' : 'Reaver Nose Cannon',         points : 0 },
+  '126': { 'category' : 'Reaver Wing Mount',          points : 0 },
+  '127': { 'category' : 'Scythe Nose Cannon',         points : 0 },
+  '128': { 'category' : 'Scythe Wing Mount',          points : 0 },
+  '129': { 'category' : 'Sunderer Front Gunner',      points : 0 },
+  '130': { 'category' : 'Sunderer Rear Gunner',       points : 0 },
+  '131': { 'category' : 'Vanguard Gunner Weapon',     points : 0 },
+  '132': { 'category' : 'Vanguard Primary Weapon',    points : 0 },
+  '139': { 'category' : 'Engineer Turrets',           points : 1 },
+  '147': { 'category' : 'Aerial Combat Weapon',       points : 0 }
+};
+
+// Emerald "DurdleDome" (2016)
+var emeraldCategoryMap = {
+    '0':   { 'category' : 'Unknown (could be anything', points : 0 },
+    '2':   { 'category' : 'Knife',                      points : 2 },
+    '3':   { 'category' : 'Pistol',                     points : 2 },
+    '4':   { 'category' : 'Shotgun',                    points : 0 },
+    '5':   { 'category' : 'SMG',                        points : 2 },
+    '6':   { 'category' : 'LMG',                        points : 2 },
+    '7':   { 'category' : 'Assault Rifle',              points : 2 },
+    '8':   { 'category' : 'Carbine',                    points : 2 },
+    '9':   { 'category' : 'AV MAX (Left)',              points : 1 },
+    '10':  { 'category' : 'AI MAX (Left)',              points : 1 },
+    '11':  { 'category' : 'Sniper Rifle',               points : 2 },
+    '12':  { 'category' : 'Scout Rifle',                points : 2 },
+    '13':  { 'category' : 'Rocket Launcher',            points : 0 },
+    '14':  { 'category' : 'Heavy Gun',                  points : 0 },
+    '15':  { 'category' : 'Flamethrower MAX',           points : 1 },
+    '16':  { 'category' : 'Flak MAX',                   points : 1 },
+    '17':  { 'category' : 'Grenade',                    points : 2 },
+    '18':  { 'category' : 'Explosive',                  points : 0 },
+    '19':  { 'category' : 'Battle Rifle',               points : 2 },
+    '20':  { 'category' : 'AA MAX (Right)',             points : 1 },
+    '21':  { 'category' : 'AV MAX (Right)',             points : 1 },
+    '22':  { 'category' : 'AI MAX (Right)',             points : 1 },
+    '23':  { 'category' : 'AA MAX (Left)',              points : 1 },
+    '24':  { 'category' : 'Crossbow',                   points : 2 },
+    '100': { 'category' : 'Infantry',                   points : 1 },
+    '101': { 'category' : 'Vehicles',                   points : 0 },
+    '102': { 'category' : 'Infantry Weapons',           points : 1 },
+    '104': { 'category' : 'Vehicle Weapons',            points : 0 },
+    '109': { 'category' : 'Flash Primary Weapon',       points : 0 },
+    '110': { 'category' : 'Galaxy Left Weapon',         points : 0 },
+    '111': { 'category' : 'Galaxy Tail Weapon',         points : 0 },
+    '112': { 'category' : 'Galaxy Right Weapon',        points : 0 },
+    '113': { 'category' : 'Galaxy Top Weapon',          points : 0 },
+    '114': { 'category' : 'Harasser Top Gunner',        points : 0 },
+    '115': { 'category' : 'Liberator Belly Weapon',     points : 0 },
+    '116': { 'category' : 'Liberator Nose Cannon',      points : 0 },
+    '117': { 'category' : 'Liberator Tail Weapon',      points : 0 },
+    '118': { 'category' : 'Lightning Primary Weapon',   points : 0 },
+    '119': { 'category' : 'Magrider Gunner Weapon',     points : 0 },
+    '120': { 'category' : 'Magrider Primary Weapon',    points : 0 },
+    '121': { 'category' : 'Mosquito Nose Cannon',       points : 0 },
+    '122': { 'category' : 'Mosquito Wing Mount',        points : 0 },
+    '123': { 'category' : 'Prowler Gunner Weapon',      points : 0 },
+    '124': { 'category' : 'Prowler Primary Weapon',     points : 0 },
+    '125': { 'category' : 'Reaver Nose Cannon',         points : 0 },
+    '126': { 'category' : 'Reaver Wing Mount',          points : 0 },
+    '127': { 'category' : 'Scythe Nose Cannon',         points : 0 },
+    '128': { 'category' : 'Scythe Wing Mount',          points : 0 },
+    '129': { 'category' : 'Sunderer Front Gunner',      points : 0 },
+    '130': { 'category' : 'Sunderer Rear Gunner',       points : 0 },
+    '131': { 'category' : 'Vanguard Gunner Weapon',     points : 0 },
+    '132': { 'category' : 'Vanguard Primary Weapon',    points : 0 },
+    '139': { 'category' : 'Engineer Turrets',           points : 0 },
+    '147': { 'category' : 'Aerial Combat Weapon',       points : 0 }
+};
+
+// Briggs OvO (2017)
+var ovoCategoryMap = {
+    '0':   { 'category' : 'Unknown (could be anything', points : 0 },
+    '2':   { 'category' : 'Knife',                      points : 2 },
+    '3':   { 'category' : 'Pistol',                     points : 2 },
+    '4':   { 'category' : 'Shotgun',                    points : 0 },
+    '5':   { 'category' : 'SMG',                        points : 2 },
+    '6':   { 'category' : 'LMG',                        points : 2 },
+    '7':   { 'category' : 'Assault Rifle',              points : 2 },
+    '8':   { 'category' : 'Carbine',                    points : 2 },
+    '9':   { 'category' : 'AV MAX (Left)',              points : 1 },
+    '10':  { 'category' : 'AI MAX (Left)',              points : 1 },
+    '11':  { 'category' : 'Sniper Rifle',               points : 2 },
+    '12':  { 'category' : 'Scout Rifle',                points : 2 },
+    '13':  { 'category' : 'Rocket Launcher',            points : 0 },
+    '14':  { 'category' : 'Heavy Gun',                  points : 0 },
+    '15':  { 'category' : 'Flamethrower MAX',           points : 0 },
+    '16':  { 'category' : 'Flak MAX',                   points : 0 },
+    '17':  { 'category' : 'Grenade',                    points : 2 },
+    '18':  { 'category' : 'Explosive',                  points : 0 },
+    '19':  { 'category' : 'Battle Rifle',               points : 2 },
+    '20':  { 'category' : 'AA MAX (Right)',             points : 1 },
+    '21':  { 'category' : 'AV MAX (Right)',             points : 1 },
+    '22':  { 'category' : 'AI MAX (Right)',             points : 1 },
+    '23':  { 'category' : 'AA MAX (Left)',              points : 1 },
+    '24':  { 'category' : 'Crossbow',                   points : 2 },
+    '100': { 'category' : 'Infantry',                   points : 1 },
+    '101': { 'category' : 'Vehicles',                   points : 0 },
+    '102': { 'category' : 'Infantry Weapons',           points : 1 },
+    '104': { 'category' : 'Vehicle Weapons',            points : 0 },
+    '109': { 'category' : 'Flash Primary Weapon',       points : 0 },
+    '110': { 'category' : 'Galaxy Left Weapon',         points : 0 },
+    '111': { 'category' : 'Galaxy Tail Weapon',         points : 0 },
+    '112': { 'category' : 'Galaxy Right Weapon',        points : 0 },
+    '113': { 'category' : 'Galaxy Top Weapon',          points : 0 },
+    '114': { 'category' : 'Harasser Top Gunner',        points : 0 },
+    '115': { 'category' : 'Liberator Belly Weapon',     points : 0 },
+    '116': { 'category' : 'Liberator Nose Cannon',      points : 0 },
+    '117': { 'category' : 'Liberator Tail Weapon',      points : 0 },
+    '118': { 'category' : 'Lightning Primary Weapon',   points : 0 },
+    '119': { 'category' : 'Magrider Gunner Weapon',     points : 0 },
+    '120': { 'category' : 'Magrider Primary Weapon',    points : 0 },
+    '121': { 'category' : 'Mosquito Nose Cannon',       points : 0 },
+    '122': { 'category' : 'Mosquito Wing Mount',        points : 0 },
+    '123': { 'category' : 'Prowler Gunner Weapon',      points : 0 },
+    '124': { 'category' : 'Prowler Primary Weapon',     points : 0 },
+    '125': { 'category' : 'Reaver Nose Cannon',         points : 0 },
+    '126': { 'category' : 'Reaver Wing Mount',          points : 0 },
+    '127': { 'category' : 'Scythe Nose Cannon',         points : 0 },
+    '128': { 'category' : 'Scythe Wing Mount',          points : 0 },
+    '129': { 'category' : 'Sunderer Front Gunner',      points : 0 },
+    '130': { 'category' : 'Sunderer Rear Gunner',       points : 0 },
+    '131': { 'category' : 'Vanguard Gunner Weapon',     points : 0 },
+    '132': { 'category' : 'Vanguard Primary Weapon',    points : 0 },
+    '139': { 'category' : 'Engineer Turrets',           points : 0 },
+    '147': { 'category' : 'Aerial Combat Weapon',       points : 0 }
 };
 
 var mItemTemplate = JSON.stringify({
@@ -86,6 +192,7 @@ var mItemTemplate = JSON.stringify({
 });
 
 function initialise() {
+  if (categoryMap === 0) { categoryMap = thunderDomeCategoryMap;}
   var response = Q.defer();
   var url = 'https://census.daybreakgames.com/s:' + api_key.KEY + '/get/ps2/item?item_type_id=26&c:limit=5000&c:hide=,skill_set_id,is_vehicle_weapon,item_type_id,faction_id,max_stack_size,image_set_id,image_path,is_default_attachment&c:lang=en';
   prequest(url).then(function (body) {
@@ -130,6 +237,14 @@ function lookupPointsFromCategory(id) {
     console.error('missing category: ' + id);
     return 0;
   }
+}
+
+function getCategoryMap() {
+  return category_map;
+}
+
+function updateCategoryMap(categoryMap) {
+  category_map = categoryMap;
 }
 
 exports.initialise = initialise;
